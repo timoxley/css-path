5c19a836088e9b4cda69ad1490c77d8e2a70d2d5
Tim Oxley <secoif@gmail.com>
2012-11-15 09:43:47 +1000

    Use nth-child in css-path.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function _cssPath(el, path) {
   path = path || [];
   if (!el || getNodeName(el) === 'html') return path
 
-  var elSelector = [getNodeName, getIdSelector, getClassSelector]
+  var elSelector = [getNodeName, getIdSelector, getClassSelector, getChildIndex]
   .map(function(func) { return func(el) }) // apply functions
   .filter(function(item) {return !!item}) // remove non-results
   .join('').trim()
@@ -17,35 +17,49 @@ function _cssPath(el, path) {
   return _cssPath(el.parentNode, path)
 }
 
-/*
+/**
  * Get element's .class .list
  * @param {Element} dom element
  * @return {String} classes of element as CSS selector
  */
+
 function getClassSelector(el) {
   return el.className && el.className.split(' ')
   .map(function(className) { return '.' + className })
   .join('')
 }
 
-/*
+/**
  * Get element #id
+ *
  * @param {Element} dom element
  * @return {String} id of element as CSS selector
  */
+
 function getIdSelector(el) {
   return el.id ? '#' + el.id : ''
 }
 
-/*
- * Get element node name
+/**
+ * Get element node name (e.g. div, li, body)
+ *
  * @param {Element} dom element
  * @return {String} node name of element as CSS selector
  */
+
 function getNodeName(el) {
   return (el.nodeName).toLowerCase()
 }
 
+/**
+ * Get CSS position of node relative to siblings, e.g. :nth-child(3)
+ *
+ * @param {Element} dom element
+ * @return {String} node name of element as CSS selector
+ */
+
 function getChildIndex(el) {
-  return 'nth-child('+ indexOf(el) +')'
+  var index = indexOf(el)
+  if (!~index || el.tagName === 'BODY') return ''
+  return ':nth-child('+ (index + 1) +')'
 }
